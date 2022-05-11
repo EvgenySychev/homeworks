3a91c54bc48443af439ab59025aacec6e4a546d2
Evgeny <samara808@uandex.ru>
2022-05-11 19:55:52 +0400

    H1

---
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior+.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function JuniorPlus() {
+    return (
+        <div>
+            {/*<HW13/>
+            <HW14/>
+            <HW15/>
+            <HW10/>
+            <HW11/>
+            <HW12/>*/}
+            Junior+
+        </div>
+    )
+}
+
+export default JuniorPlus
+
+// сделайте по аналогии пустые страницы джун и джун+
+// туда будут добавляться следующие дз
---
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Junior() {
+    return (
+        <div>
+            {/*<HW7/>
+            <HW8/>
+            <HW9/>
+            <HW10/>
+            <HW11/>
+            <HW12/>*/}
+            Junior
+        </div>
+    )
+}
+
+export default Junior
+
+// сделайте по аналогии пустые страницы джун и джун+
+// туда будут добавляться следующие дз
