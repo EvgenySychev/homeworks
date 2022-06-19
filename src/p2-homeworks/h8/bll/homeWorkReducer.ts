ec2f20e2bda15c07926179f4939be153b5bccec1
EvgenySychev <samara808@yandex.ru>
2022-06-19 17:38:34 +0400

    H6

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,25 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+type ActionType = {type: 'sort' | 'check', payload: 'up' | 'down'} | {type: 'sort' | 'check', payload: 18}
+
+
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Array<UserType> => {
+
     switch (action.type) {
+
         case 'sort': {
-            // need to fix
-            return state
+            if (action.payload === 'down') {
+                return [...state].sort((a,b)=> a.age-b.age)
+            }
+            if (action.payload === 'up') {
+                return [...state].sort((a,b)=> b.age-a.age)
+            }
+
         }
         case 'check': {
-            // need to fix
-            return state
+            return state.filter(t => t.age>18).sort((a,b) => b.age-a.age)
         }
         default: return state
     }
 }
+
