b1a8f74799bf9735eca1e46405816d1d391e4daf
EvgenySychev <samara808@yandex.ru>
2022-06-19 22:10:11 +0400

    H9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,31 +1,33 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
+const get2digitsString = (num: number) => num < 10 ? "0" + num : num
+
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = `${get2digitsString(date.getHours())}:${get2digitsString(date.getMinutes())}:${get2digitsString(date.getSeconds())}` // date?.tiLocalTimeString()
+    const stringDate = `${get2digitsString(date.getDate())}.${get2digitsString(date.getMonth())}.${date.getFullYear()}` // date?.tiLocalDateString()
 
     return (
         <div>
@@ -36,11 +38,9 @@ function Clock() {
                 {stringTime}
             </div>
 
-            {show && (
-                <div>
-                    {stringDate}
-                </div>
-            )}
+            {show ? <div> {stringDate} </div> : <br/>}
+
+
 
             <SuperButton onClick={start}>start</SuperButton>
             <SuperButton onClick={stop}>stop</SuperButton>
