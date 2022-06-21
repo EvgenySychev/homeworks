6b4ace519a854e37ca24eca3e180aa157e56d652
EvgenySychev <samara808@yandex.ru>
2022-06-21 22:41:20 +0400

    H10

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "./bll/store";
+import {loadingAC} from "./bll/loadingReducer";
+import {Preloader} from "./Preloader";
 
 function HW10() {
-    // useSelector, useDispatch
-    const loading = false
+    let dispatch = useDispatch()
+    let loading = useSelector<AppStoreType, boolean>(state => state.onLoading.loading)
 
     const setLoading = () => {
-        // dispatch
-        // setTimeout
+        dispatch(loadingAC(true))
+        setTimeout(() => {
+            dispatch(loadingAC(false))
+        }, 3000)
         console.log('loading...')
     };
 
@@ -19,7 +25,7 @@ function HW10() {
             {/*should work (должно работать)*/}
             {loading
                 ? (
-                    <div>крутилка...</div>
+                    <Preloader/>
                 ) : (
                     <div>
                         <SuperButton onClick={setLoading}>set loading...</SuperButton>
--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,22 @@
-const initState = {
 
+type initStateType = {
+    loading: boolean
+}
+type loadingACType = ReturnType<typeof loadingAC>
+type ActionType = loadingACType
+
+const initState = {
+    loading: false
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: ActionType): initStateType => {
     switch (action.type) {
-        case '': {
-            return state
+        case "START-LOADING" : {
+            return {...state, loading:action.load}
+
         }
         default: return state
     }
 }
 
-export const loadingAC = (): any => {} // fix any
+export const loadingAC = (load: boolean) => ({type: "START-LOADING",load} as const)
